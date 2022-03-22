1d40f3165a452976732984d70491890f2bb22dc5
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-03-22 11:10:15 +0000

    react events

--- src/component/ProfileCard.js
+++ src/component/ProfileCard.js
@@ -9,17 +9,24 @@ setShowBio(!showBio);
 };
 
   return (
-
-
     <div className='card'>
+    <div className='cardcontent'>
+
+{showBio? (
+<div className='biowrap'>
+<p className='bio'>{writer.bio}</p>
+</div>
+):(
+
     <img src= {`images/${writer.avatar}.png`}
      height="300px" width="300px" alt={writer.img}/>
-    
+  )} 
+  </div> 
     <div className='textGroup'>
     <h3>{writer.name}</h3>
     <p>{writer.email}</p>
     <p>{writer.phone}</p>
-    <button className='actionBtn' >Read Bio</button>
+    <button className='actionBtn' onClick={() =>handleClick(writer.bio)} >Read Bio</button>
 
     </div>
  
