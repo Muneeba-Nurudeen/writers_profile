89dcc36ffdbcb7b84ccef4e1eef7df163314e5b3
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-03-22 09:37:47 +0000

    added new data

--- src/component/ProfileCard.js
+++ src/component/ProfileCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function ProfileCard({writer}) {
   return (
-    <div className='card'>
-    <img src= {` images/${writer.avatar}.png `} height="300px" width="300px" alt={writer.img}/>
+    <div className='cards'>
+    <img src= {`images/${writer.avatar}.png`} height="300px" width="300px" alt={writer.img}/>
     
     <div className='textGroup'>
     <h3>{writer.name}</h3>
     <p>{writer.email}</p>
     <p>{writer.phone}</p>
-    <button className='actionBtn' onClick={this.handleClick}s>Read Bio</button>
+    <button className='actionBtn' >Read Bio</button>
 
     </div>
  
@@ -17,4 +17,4 @@ function ProfileCard({writer}) {
   )
 }
 
-export default ProfileCard
+export default ProfileCard;
